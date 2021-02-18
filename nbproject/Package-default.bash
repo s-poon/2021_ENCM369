827fdeaa31af6a6c0167bf2342dd031c96b191f7
s-poon <stevenpoon12.03@gmail.com>
2021-02-18 13:26:44 -0700

    Adding Project Files and deleting old README

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PIC_Activity4.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=PIC_Activity4.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=picactivity4.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/2021_ENCM369.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=2021_ENCM369.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=2021encm369/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/picactivity4.x/bin
+makeDirectory ${TMPDIR}/2021encm369/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/picactivity4.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/2021encm369.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/picactivity4.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/2021encm369.tar *
 checkReturnCode
 
 # Cleanup
